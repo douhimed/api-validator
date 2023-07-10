96bc75927eab89f1f22c6b2a30071e2357d77478
ElMourabit-Mohammed <elmourabitmohammed00@gmail.com>
2023-07-10 10:05:17 +0100

    [feature]VLD-6:swagger documentation

--- src/main/java/com/sqli/intern/api/validator/Application.java
+++ src/main/java/com/sqli/intern/api/validator/Application.java
@@ -1,9 +1,14 @@
 package com.sqli.intern.api.validator;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Api Validation" ,
+                                version = "1.0",
+                                description ="Application for API validation"))
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/sqli/intern/api/validator/client/rest/WelcomeRest.java
+++ src/main/java/com/sqli/intern/api/validator/client/rest/WelcomeRest.java
@@ -3,6 +3,11 @@ package com.sqli.intern.api.validator.client.rest;
 import com.sqli.intern.api.validator.services.OperationService;
 import com.sqli.intern.api.validator.utilities.dtos.RequestDto;
 import com.sqli.intern.api.validator.utilities.enums.ValidationStatus;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,16 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@Tag(name = "Welcome Rest", description = "Endpoints related to API Validation")
 public class WelcomeRest {
     @Autowired
     private OperationService operationService;
 
     @GetMapping("/welcome")
+    @Operation(summary = "Welcome message", description = "Returns a welcome message")
     public String welcome() {
         return "Welcome";
     }
 
     @PostMapping("/validator")
+    @Operation(summary = "JSON validator", description = "Validates a JSON request compares the expect json with the actual json")
+    @ApiResponse(responseCode = "200", description = "Validation status",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ValidationStatus.class)))
     public ValidationStatus jsonValidator(@RequestBody RequestDto requestDto) {
         return operationService.compareJson(requestDto);
     }
