c365f75ab69951fdd1e55c18b424ab423864b144
mdouhi <mdouhi@sqli.com>
2023-07-14 23:38:44 +0100

    conf

--- src/main/java/com/sqli/intern/api/validator/Application.java
+++ src/main/java/com/sqli/intern/api/validator/Application.java
@@ -4,12 +4,19 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Api Validation" ,
                                 version = "1.0",
                                 description ="Application for API validation"))
-public class Application  {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
