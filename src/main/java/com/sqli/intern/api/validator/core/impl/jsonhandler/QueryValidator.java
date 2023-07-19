7e325653dba73277641966b097bdc07bc28239da
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-07-19 18:55:51 +0100

    [feat]VLD-15: filtrage des op de type move

--- src/main/java/com/sqli/intern/api/validator/core/impl/jsonhandler/QueryValidator.java
+++ src/main/java/com/sqli/intern/api/validator/core/impl/jsonhandler/QueryValidator.java
@@ -3,12 +3,15 @@ package com.sqli.intern.api.validator.core.impl.jsonhandler;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.flipkart.zjsonpatch.JsonDiff;
+import com.sqli.intern.api.validator.utilities.JsonUtils;
 import com.sqli.intern.api.validator.utilities.dtos.ResponseDto;
 import com.sqli.intern.api.validator.utilities.enums.ValidationStatus;
 import org.springframework.stereotype.Component;
 
 @Component
 public class QueryValidator extends JsonHandler {
+    private static final String PATH = "/op";
+    private static final String VALUE = "move";
 
 
     @Override
@@ -19,13 +22,18 @@ public class QueryValidator extends JsonHandler {
             if (patch.size() == 0) {
                 responseDto.setValidationStatus(ValidationStatus.VALID);
             } else {
-                patch.forEach(node -> responseDto.addMessage(node.toString()));
+                patch.forEach(node -> {
+                    if (JsonUtils.estNodeValueNotEquals(node, PATH, VALUE)) {
+                        responseDto.addMessage(node.toString());
+                    }
+                });
                 responseDto.setValidationStatus(ValidationStatus.INVALID);
             }
         } catch (JsonProcessingException e) {
             responseDto.addMessage("INVALID FORMAT");
         }
     }
+
     public void compareJson(ResponseDto responseDto) {
         invoke(responseDto);
     }
