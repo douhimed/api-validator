3e1b69abf6bab047bcbe33031d120c0c9da1450f
ElMourabit-Mohammed <elmourabitmohammed00@gmail.com>
2023-07-21 15:14:15 +0100

    [fix]VLD-16: bug resolved

--- src/main/java/com/sqli/intern/api/validator/utilities/mappers/ReportMapper.java
+++ src/main/java/com/sqli/intern/api/validator/utilities/mappers/ReportMapper.java
@@ -8,9 +8,9 @@ public final class ReportMapper {
     private ReportMapper() {throw new RuntimeException("INSTANTIATION NOT ALLOWED");}
 
         public static ReportDto map(JsonNode node) {
-            String operation = node.get("op").toString();
-            String path = node.get("path").toString();
-            String value = node.get("value").toString();
+            String operation = node.has("op") ? node.get("op").asText() : null;
+            String path = node.has("path") ? node.get("path").asText() : null;
+            String value = node.has("value") ? node.get("value").toString() : null;
 
             return ReportDto.builder()
                     .operation(operation)
