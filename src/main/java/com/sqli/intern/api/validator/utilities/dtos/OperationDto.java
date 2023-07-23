205fad84babaf18e2ad16d4b7647f293433994a5
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-07-23 17:32:27 +0100

    rework-3 fessdback 2

--- src/main/java/com/sqli/intern/api/validator/utilities/dtos/OperationDto.java
+++ src/main/java/com/sqli/intern/api/validator/utilities/dtos/OperationDto.java
@@ -21,6 +21,7 @@ public class OperationDto {
     @NotBlank(message = "Body must not be blank")
     @JsonValid(message = "Body not valid")
     private String body;
+    @JsonValid
     private String expectedResponse;
     private String actualResponse;
     private String expectedType;
