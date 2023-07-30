2ad5937d07050bdc91f292a29f7db457a6674467
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-07-30 13:44:20 +0100

    mapping project

--- src/main/java/com/sqli/intern/api/validator/utilities/mappers/OperationMapper.java
+++ src/main/java/com/sqli/intern/api/validator/utilities/mappers/OperationMapper.java
@@ -1,6 +1,7 @@
 package com.sqli.intern.api.validator.utilities.mappers;
 
 import com.sqli.intern.api.validator.entities.OperationEntity;
+import com.sqli.intern.api.validator.entities.ProjectEntity;
 import com.sqli.intern.api.validator.utilities.dtos.OperationDto;
 
 import java.util.List;
@@ -37,6 +38,7 @@ public final class OperationMapper {
                 .expectedResponse(operationDto.getExpectedResponse())
                 .actualResponse(operationDto.getActualResponse())
                 .expectedType(operationDto.getExpectedType())
+                .project(ProjectEntity.builder().id(operationDto.getProjectId()).build())
                 .build();
     }
 
