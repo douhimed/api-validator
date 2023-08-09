f4a47b4afefa39f96768ff2133091d001d9430b7
ElMourabit-Mohammed <elmourabitmohammed00@gmail.com>
2023-08-09 15:23:51 +0100

    [fix]VALUE-FIX: fix the value problem

--- src/main/java/com/sqli/intern/api/validator/utilities/dtos/ReportDto.java
+++ src/main/java/com/sqli/intern/api/validator/utilities/dtos/ReportDto.java
@@ -2,6 +2,9 @@ package com.sqli.intern.api.validator.utilities.dtos;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.*;
 
 
@@ -17,15 +20,15 @@ public class ReportDto {
     private String path;
     @JsonProperty("value")
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private Object value;
+    private JsonNode value;
 
 
     public static ReportDto createErrorMessage(String errorMsg) {
+        ObjectNode valueNode = JsonNodeFactory.instance.objectNode();
+        valueNode.put("msg", errorMsg);
+
         return ReportDto.builder()
-                .value(ValueDto.builder()
-                        .msg(errorMsg)
-                        .build()
-                        .toString())
+                .value(valueNode)
                 .build();
     }
 }
